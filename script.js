948e60be52cb51c5f6cad7c522cb2c7bd5b6e41f
Matthew Aileru <m.a.aileru@gmail.com>
2021-11-07 23:04:08 +0000

    Update script.js

--- script.js
+++ script.js
@@ -46,9 +46,159 @@ function fetchFoundCard(card) {
 
 function displayFoundCard(cards) {
     let foundCard = cards.find(fetchFoundCard);
-    
-    cardsDisplay.innerHTML = `<div>${foundCard.Name}</div>
-                            <div><img src=${foundCard.ImageUrl} alt="Image of ${foundCard.Name}" width="200px" height="300px" /></div>`;
+    /**
+    cardsDisplay.innerHTML = `<div class="cell">${foundCard.Name}</div>
+    <div class="cell"><img src=${foundCard.ImageUrl} alt="Image of ${foundCard.Name}" width="200px" height="300px" /></div>
+    <div class="cell">Card Cost</div><div class="cell">${foundCard.Cost}</div>
+    <div class="cell">Influence</div><div class="cell">${foundCard.Influence}</div>
+    <div class="cell">Attack</div><div class="cell">${foundCard.Attack}</div>
+    <div class="cell">Health</div><div class="cell">${foundCard.Health}</div>
+    <div class="cell">Rarity</div><div class="cell">${foundCard.Rarity}</div>
+    <div class="cell">Type</div><div class="cell">${foundCard.Type}</div>
+    <div class="cell">Unit Type</div><div class="cell">${foundCard.UnitType}</div>
+    <div class="cell">Deck Buildable?</div><div class="cell">${foundCard.DeckBuildable}</div>
+    <div class="cell">More Details Link</div><div class="cell">${foundCard.DetailsUrl}</div>`;
+ */
+//     cardsDisplay.innerHTML = 
+//     `<table class="mobile-table">
+//       <tr>
+//         <th>Name</th>
+//       </tr>
+//       <tr>
+//         <td>${foundCard.Name}</td>
+//       </tr>
+//       <tr>
+//         <th>Card Image</th>
+//       </tr>
+//       <tr>
+//         <td><img src=${foundCard.ImageUrl} alt="Image of ${foundCard.Name}" width="200px" height="300px" /></td>
+//       <tr>
+//       <tr>
+//          <th>Card Cost</th> 
+//       </tr>
+//       <tr>
+//          <td>${foundCard.Cost}</td>
+//       </tr>
+//       <tr>
+//          <th>Influence</th> 
+//       </tr>
+//       <tr>
+//         <td>${foundCard.Influence}</td>
+//       </tr>
+//       <tr>
+//         <th>Attack</th> 
+//       </tr>
+//       <tr>
+//         <td>${foundCard.Attack}</td>
+//       </tr>
+//       <tr>
+//          <th>Health</th> 
+//       </tr>
+//       <tr>
+//         <td>${foundCard.Health}</td>
+//       </tr>
+//       <tr>
+//          <th>Rarity</th> 
+//       </tr>
+//       <tr>
+//         <td>${foundCard.Rarity}</td>
+//       </tr>
+//       <tr>
+//          <th>Type</th> 
+//       </tr>
+//       <tr>
+//         <td>${foundCard.Type}</td>
+//       </tr>
+//       <tr>
+//         <th>Unit Type</th> 
+//       </tr>
+//       <tr>
+//         <td>${foundCard.UnitType}</td>
+//       </tr>
+//       <tr>
+//         <th>Deck Buildable?</th> 
+//       </tr>
+//       <tr>
+//         <td>${foundCard.DeckBuildable}</td>
+//       </tr>
+//       <tr>
+//         <th>More Details</th> 
+//       </tr>
+//       <tr>
+//         <td>${foundCard.DetailsUrl}</td>
+//       </tr>
+//     </table>`;
+// }
+
+cardsDisplay.innerHTML = 
+    `<div class="mobile-table">
+      <div class="heading">
+        <h4>Name</h4>
+      </div>
+      <div class="card-data">
+        <p>${foundCard.Name}</p>
+      </div>
+      <div class="heading">
+        <h4>Card Image</h4>
+      </div>
+      <div class="card-data">
+        <img src=${foundCard.ImageUrl} alt="Image of ${foundCard.Name}" width="200px" height="300px" />
+      </div>
+      <div class="heading">
+         <h4>Card Cost</h4> 
+      </div>
+      <div class="card-data">
+         <p>${foundCard.Cost}</p>
+      </div>
+      <div class="heading">
+         <h4>Influence</h4> 
+      </div>
+      <div class="card-data">
+        <p>${foundCard.Influence}</p>
+      </div>
+      <div class="heading">
+        <h4>Attack</h4> 
+      </div>
+      <div class="card-data">
+        <p>${foundCard.Attack}</p>
+      </div>
+      <div class="heading">
+         <h4>Health</h4> 
+      </div>
+      <div class="card-data">
+        <p>${foundCard.Health}</p>
+      </div>
+      <div class="heading">
+         <h4>Rarity</h4> 
+      </div>
+      <div class="card-data">
+        <p>${foundCard.Rarity}</p>
+      </div>
+      <div class="heading">
+         <h4>Type</h4> 
+      </div>
+      <div class="card-data">
+        <p>${foundCard.Type}</p>
+      </div>
+      <div class="heading">
+        <h4>Unit Type</h4> 
+      </div>
+      <div class="card-data">
+        <p>${foundCard.UnitType}</p>
+      </div>
+      <div class="heading">
+        <h4>Deck Buildable?</h4> 
+      </div>
+      <div class="card-data">
+        <p>${foundCard.DeckBuildable}</p>
+      </div>
+      <div class="heading">
+        <h4>More Details</h4> 
+      </div>
+      <div class="card-data">
+        <p>${foundCard.DetailsUrl}</p>
+      </div>
+    </div>`;
 }
 
 /*
@@ -81,9 +231,8 @@ function displayShows(shows) {
 // searchBar.addEventListener("change", () => { getData().then(c => cardsDisplay.innerHTML = c.find(fetchFoundCard).Name).catch(e => console.log(e)) } );
 // findButton.addEventListener("submit", () => { getData().then(c => cardsDisplay.innerHTML = c.find(fetchFoundCard).Name).catch(e => console.error(e)) } );
 
-searchBar.addEventListener("change", () => { getData().then(c => displayFoundCard(c)).catch(e => console.log(e)) } );
+searchBar.addEventListener("change", () => { getData().then(c => displayFoundCard(c)).catch(e => console.error(e)) } );
 findButton.addEventListener("submit", () => { getData().then(c => displayFoundCard(c)).catch(e => console.error(e)) } );
-
 
 
 // getData().then(c => displayFoundCard(c[0])).catch(e => console.log(e));
